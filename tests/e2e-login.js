a9523b4d5381bd4cdcef9e74ebd82e3e6e872106
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2020-09-05 16:33:53 -0500

    login and logout tests completed

--- tests/e2e-login.js
+++ tests/e2e-login.js
@@ -5,20 +5,30 @@ describe('Should test for login credentials', () => {
         $('#signin_button').waitForExist()
         $('#signin_button').click()
         $('#login_form').waitForExist()
-        $('#user_login').setValue('Username')
-        $('#user_password').setValue('Password')
+        $('#user_login').setValue('invalid')
+        $('#user_password').setValue('invalid')
         $('input[type="submit"]').click()
         const error = $('.alert-error')
         expect(error).toHaveText('Login and/or password are wrong.')
     })
 
     it('Should login successfully', () => {
-
-
+        browser.url('http://zero.webappsecurity.com/index.html')
+        $('#signin_button').waitForExist()
+        $('#signin_button').click()
+        $('#login_form').waitForExist()
+        $('#user_login').setValue('username')
+        $('#user_password').setValue('password')
+        $('input[type="submit"]').click()
+        $('.nav-tabs').waitForExist()
     })
 
     it('Should log out the user', () => {
         
-
+        $('.icon-user').waitForExist()
+        $('.icon-user').click()
+        $('#logout_link').waitForExist()
+        $('#logout_link').click()
+        $('.navbar-fixed-top').waitForExist()
     })
 })
